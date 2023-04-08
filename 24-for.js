6bf72a61f033d24f6c4f21b2527a05c882d032dc
devthiagocesar <devthiagocesar@gmail.com>
2023-04-08 11:38:21 -0300

    080423

---
+++ 24-for.js
@@ -0,0 +1,12 @@
+/*
+ESTRUTURA DE REPETIÇÃO - FOR
+*/
+
+
+const students = ['Bruna', 'Ana', 'João', 'Maria', 'Pedro']
+
+
+ for (let i = 0; i < students.length; i++) {
+console.log (`${students[i]} está presente a aula`)
+    
+ }
