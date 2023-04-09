818c5a54c8024c2bc885160ef15cb4b369310008
devthiagocesar <devthiagocesar@gmail.com>
2023-04-08 21:58:10 -0300

    080423

---
+++ 26-for-of.js
@@ -0,0 +1,10 @@
+/* ESTRUTURAS DE REPETIÇÃO - FOR OF
+*/
+
+let myName = 'Thiago'
+let allNames =['Maria', 'João', 'Pedro', 'Gilberto']
+
+for (let letter of allNames) {
+    
+    console.log (letter)
+}
